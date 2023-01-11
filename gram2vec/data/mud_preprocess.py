0ad512c59fdb9f4285f4276d0d36791d7a734e7a
eric <eric.sclafani321@gmail.com>
2023-01-11 13:00:07 -0500

    began MUD extraction

--- gram2vec/data/mud_preprocess.py
+++ gram2vec/data/mud_preprocess.py
@@ -1,4 +1,38 @@
 #!/usr/bin/env python3
 
+import ijson
 
 
+def extract_n_authors_k_entries(raw_path, n, k):
+    """This function extracts n amount of authors from MUD with k amount of documents"""
+    data = ijson.parse(open(raw_path), multiple_values=True)
+
+    seen_authors = 0
+
+    data = {}
+    posts = []
+    for prefix, _, value in data:
+        
+        if prefix == "syms.item":
+            posts.append(value)
+            
+        if prefix.startswith("author_id") and len(posts) >= k:
+            data[value] = posts
+            posts = []
+            seen_authors += 1
+
+        if seen_authors == n:
+            break
+        
+    return data
+
+
+
+def main():
+    pass
+
+
+
+
+if __name__ == "__main__":
+    main()
--- gram2vec/eval_bins.sh
+++ gram2vec/eval_bins.sh
@@ -1,5 +1,8 @@
 #!/bin/env bash
 
+# this script evaluates the knn classifier on development bins for PAN 2022
+
+
 for json in data/pan/dev_bins/*; do
 
     if [ -f "$json" ]; then
