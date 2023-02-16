7de81c5513652b6e8b9406f9684f0d6bdd0e2e32
eric-sclafani <eric.sclafani321@gmail.com>
2023-02-16 13:49:23 -0500

    set up metric learning, training bottlenecks my CPU

--- gram2vec/train_mahalanobis.py
+++ gram2vec/train_mahalanobis.py
@@ -1,18 +1,76 @@
 from metric_learn import MMC
+import numpy as np
+import pickle
+import os
+import jsonlines
+import argparse
+from dataclasses import dataclass
+from tqdm import tqdm
 
-pairs = [[[1.2, 7.5], [1.3, 1.5]],
+# project imports
+from featurizers import GrammarVectorizer
+import utils
 
-         [[6.4, 2.6], [6.2, 9.7]],
 
-         [[1.3, 4.5], [3.2, 4.6]],
+@dataclass
+class Pair:
+    docs:tuple[str, str]
+    same:bool
 
-         [[6.2, 5.5], [5.4, 5.4]]]
 
-y = [1,1,-1,-1]
+def load_metric_data(path) -> list[Pair]:
+    """Reads in a jsonlines file and returns a list of Pair objects"""
+    data:list[Pair] = []
+    with jsonlines.open(path, "r") as reader:
+        for obj in reader:
+            data.append(Pair(obj["pair"], obj["same"]))
+    return data
 
-# in this task we want points where the first feature is close to be
-# closer to each other, no matter how close the second feature is
+def vectorize_pair(g2v:GrammarVectorizer, pair:Pair) -> np.ndarray:
+    """Applies the GrammarVectorizer to pairs of documents and returns a matrix of docuemnt vector pairs"""
+    vec1 = g2v.vectorize(pair.docs[0])
+    vec2 = g2v.vectorize(pair.docs[1])
+    return np.array([vec1, vec2])
 
-mmc = MMC()
+def get_pair_matrix(g2v:GrammarVectorizer, data:list[Pair]) -> np.ndarray:
+    """Returns a 3-d matrix representing a collection of vector pairs"""
+    vector_pairs = []
+    for pair in tqdm(data, desc="Vectorizing document pairs"):
+        vector_pairs.append(vectorize_pair(g2v, pair))
+    return np.stack(vector_pairs)
 
-mmc.fit(pairs, y)
+def encode_labels(train:list[Pair]) -> np.ndarray:
+    """
+    Encodes True or False labels indicating same or different author pairs, respectively
+    1 = True
+    -1 = False
+    """
+    truth_labels = [1 if pair.same == True else -1 for pair in train]
+    return np.array(truth_labels)
+          
+@utils.timer_func          
+def main():
+    
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument("-t",
+                        "--train_path",
+                        type=str,
+                        help="path to metric learning train data",
+                        default="data/pan/train_dev_test/pairs/metric_train.jsonl")
+    
+    args = parser.parse_args()
+    
+    g2v = GrammarVectorizer()
+    mmc = MMC()
+    
+    train = load_metric_data(args.train_path)
+    pair_matrix = get_pair_matrix(g2v, train)
+    truths = encode_labels(train)
+
+    print("Fitting metric learning model...")
+    mmc.fit(pair_matrix, truths)
+    
+
+if __name__ == "__main__":
+    main()
