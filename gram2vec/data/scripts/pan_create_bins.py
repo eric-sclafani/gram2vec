2e876f3c2617252407ff59d30e88ef10b24d208b
eric-sclafani <eric.sclafani321@gmail.com>
2023-02-24 13:51:50 -0500

    fixed bin script

--- gram2vec/data/scripts/pan_create_bins.py
+++ gram2vec/data/scripts/pan_create_bins.py
@@ -5,6 +5,7 @@ import jsonlines
 from pathlib import Path
 from collections import defaultdict
 import re
+from more_itertools import chunked
 import pandas as pd
 
 def iter_author_jsonls(author_files_dir:str) -> str:
@@ -26,9 +27,9 @@ def clean_file_name(file_name:str) -> str:
     """Removes extra path information from jsonl file name"""
     return re.sub(r"(eval/pan22_splits/knn/train/)|.jsonl", "", str(file_name))
 
-def author_to_doc_counts(train_dir_path:str) -> pd.DataFrame:
+def author_doc_counts_df(train_dir_path:str) -> pd.DataFrame:
     """
-    Maps each author_id to their document frequency in training set
+    Stores each unique author_id with the document count
     
     :param train_dir_path: training data directory
     :returns: dataframe consisting of author_ids and corresponding document counts
@@ -39,18 +40,39 @@ def author_to_doc_counts(train_dir_path:str) -> pd.DataFrame:
         author_docfreq_map["doc_count"].append(count_num_entries(author_file))
     return pd.DataFrame(author_docfreq_map)
 
-def get_sorted_authors_ids(train_dir_path:str) -> list[str]:
+def get_sorted_authors_ids(train_dir:str) -> list[str]:
+    """
+    Sorts a dataframe by doc_count and returns the sorted author_id labels
     
-    df = author_to_doc_counts(train_dir_path)
+    :param train_dir_path: training data directory
+    :returns: list of author_ids sorted by document frequency
+    """
+    df = author_doc_counts_df(train_dir)
     df.sort_values("doc_count", inplace=True)
-    return df.author_id.to_list()
+    return df.author_id.values
 
+def create_bins_by_docfreq(source_dir:str, target_dir:str, bins:list[list]):
+    """
+    Writes each bin to file given source and target directories, and list of bins
+    
+    :param source_dir: directory to create bins from
+    :param target_dir: directory to place the bins
+    """
+    for i, author_id_bin in enumerate(bins):
+        for author_id in author_id_bin:
+            os.system(f"cp {source_dir}/{author_id}.jsonl {target_dir}/bin{i+1}/")
+            
+            
 def main():
     
     os.chdir("../../")
+    authors_sorted_by_docfreq = get_sorted_authors_ids("eval/pan22_splits/knn/train")
+    bins_sorted_by_docfreq = list(chunked(authors_sorted_by_docfreq, 7))
+    create_bins_by_docfreq("eval/pan22_splits/knn/dev", "eval/eval_bins", bins_sorted_by_docfreq)
     
-    authors_sorted_by_train = get_sorted_authors_ids("eval/pan22_splits/knn/train")
-    print(authors_sorted_by_train)
+    
+    
+            
     
     
     
