97cf90673dd1c26117b0c5a73624c20145904869
eric-sclafani <eric.sclafani321@gmail.com>
2023-03-28 21:32:26 -0400

    changed datasets generation

--- gram2vec/make_dataset_dicts.py
+++ gram2vec/make_dataset_dicts.py
@@ -2,24 +2,35 @@ import datasets
 import os
 import pandas as pd
 import json
+from contextlib import contextmanager
 
 from featurizers import GrammarVectorizer
 g2v = GrammarVectorizer()
 
-queries = []
-candidates = []
-with open('data/hrs/queries_test.jsonl', 'r') as f:
-    for line in f:
-        queries.append(json.loads(line))
-        
-with open('data/hrs/candidates_test.jsonl', 'r') as f:
-    for line in f:
-        candidates.append(json.loads(line))
-        
-queries = pd.DataFrame(queries)
-candidates = pd.DataFrame(candidates)
+CANDIDATES_PATH = "hiatus-metrics/eval_samples_HRSBackground/mode_multi-genre/sample2_randomSeed1266677/TA1/multi-genre/multi-genre_TA1_candidates.jsonl"
+QUERIES_PATH = "hiatus-metrics/eval_samples_HRSBackground/mode_multi-genre/sample2_randomSeed1266677/TA1/multi-genre/multi-genre_TA1_queries.jsonl"
 
 
+
+@contextmanager
+def cwd(path):
+    oldpwd = os.getcwd()
+    os.chdir(path)
+    try:
+        yield
+    finally:
+        os.chdir(oldpwd)
+
+def load_hrs(path):
+    """Loads in the .jsonl Q or C documents"""
+    assert path.endswith(".jsonl")
+    with open(path) as fin:
+        return [json.loads(line) for line in fin]
+
+
+with cwd("../../../hiatus/"):
+    queries = pd.DataFrame(load_hrs(QUERIES_PATH))
+    candidates = pd.DataFrame(load_hrs(CANDIDATES_PATH))
 
 queries_docs = queries["fullText"]
 features_q = g2v.vectorize_episode(queries_docs).tolist()
@@ -32,5 +43,5 @@ candidates['features'] = features_c
 datadict = {'queries': datasets.Dataset.from_pandas(queries), 'candidates': datasets.Dataset.from_pandas(candidates)}
 data = datasets.DatasetDict(datadict)
 
-os.chdir("../../hiatus-metrics/")
-data.save_to_disk('TA1_features')
+with cwd("../../../hiatus/hiatus-metrics/"):
+    data.save_to_disk('TA1_features')
