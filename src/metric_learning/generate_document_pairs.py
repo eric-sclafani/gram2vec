6df3bf3151269bfbce750d767c1a8190d1d36e22
eric-sclafani <eric.sclafani321@gmail.com>
2023-07-18 19:50:22 -0400

    debugging calculations

--- src/metric_learning/generate_document_pairs.py
+++ src/metric_learning/generate_document_pairs.py
@@ -3,12 +3,12 @@
 import argparse
 import pandas as pd
 import numpy as np
-from itertools import combinations
-from typing import Iterable
+from typing import Iterable, List, Tuple
+from more_itertools import distinct_combinations
 
 from gram2vec import vectorizer
 
-def load_data(path) -> pd.DataFrame:
+def load_data(path:str) -> pd.DataFrame:
     df = vectorizer.load_jsonlines(path)
     df["authorIDs"] = df["authorIDs"].apply(lambda x: "".join(x))
     return df
@@ -19,11 +19,23 @@ def get_unique_author_ids(df:pd.DataFrame) -> np.ndarray[str]:
 def get_author_docs(df:pd.DataFrame, author_id:str) -> pd.Series:
     return df["fullText"].loc[df["authorIDs"] == author_id]
 
-def apply_vectorizer(documents) -> pd.DataFrame:
+def apply_vectorizer(documents:Iterable) -> np.ndarray:
     return vectorizer.from_documents(documents).values
 
-def get_all_possible_combinations(vectors:Iterable[np.ndarray]):
-    return combinations(vectors, r=2)
+def to_array(iter:List[float]) -> np.ndarray:
+    return np.array(iter)
+
+def calculate_difference(pair:Tuple[List,List]):
+    """|a-b|/2 """
+    ar1 = to_array(pair[0])
+    ar2 = to_array(pair[1])
+    return np.abs(ar1 - ar2) / 2
+
+def calculate_similarity(pair:Tuple[List,List]):
+    """1-(|a-b|/2)"""
+    return 1 - calculate_difference(pair)
+
+
 
 
 
@@ -37,13 +49,21 @@ def main():
     
     args = parser.parse_args()
     data = load_data(args.dataset_dir)
-    author_ids = get_unique_author_ids(data)    
+    author_ids = get_unique_author_ids(data)
+    
     for author_id in author_ids:
         documents = get_author_docs(data, author_id)
-        vectors = apply_vectorizer(documents)
-        combos = list(combinations(vectors, 2))
-        
+        vectors = apply_vectorizer(documents).tolist() # more_itertools.distinct_combinations crashes when working with np arrays
+        same_author_vector_pairs = distinct_combinations(vectors, r=2)
         break
+    
+    a = [15,5,5]
+    b = [2,2,2]
+    
+    result = calculate_difference((a,b))
+    print(result)
+    
+    
 
 
 if __name__ == "__main__":
